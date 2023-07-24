ae66b8fb38ef6113bfd79f0104ba8475ddfbf812
youssefkamel <kamelyoussef1996@gmail.com>
2023-07-24 01:05:40 -0400

    add keywords extraction

--- Functions.py
+++ Functions.py
@@ -1,58 +1,91 @@
-import fitz
 from unidecode import unidecode
 import re
 import pandas as pd
 import numpy as np
+import unicodedata
+import contractions
+import string
+import nltk
+import yake
 
-def Extract_text(doc):
+
+def extract_text(doc):
     output = []
     raw = ""
     for page in doc:
         output += page.get_text("blocks")
     for block in output:
-        if block[6] == 0: # We only take the text
-               plain_text = unidecode(block[4]) # Encode in ASCII
-               raw += plain_text
+        if block[6] == 0:  # We only take the text
+            plain_text = unidecode(block[4])  # Encode in ASCII
+            raw += plain_text
     return raw
 
-def Extract_dict(doc):
+
+def extract_dict(doc):
     block_dict = {}
     page_num = 1
-    for page in doc: # Iterate all pages in the document
-        file_dict = page.get_text('dict') # Get the page dictionary
-        block = file_dict['blocks'] # Get the block information
-        block_dict[page_num] = block # Store in block dictionary
-        page_num += 1 # Increase the page value by 1
+    for page in doc:  # Iterate all pages in the document
+        file_dict = page.get_text("dict")  # Get the page dictionary
+        block = file_dict["blocks"]  # Get the block information
+        block_dict[page_num] = block  # Store in block dictionary
+        page_num += 1  # Increase the page value by 1
     return block_dict
 
-def Extract_spans(doc):
-    spans = pd.DataFrame(columns=['xmin', 'ymin', 'xmax', 'ymax', 'text', 'tag'])
+
+def extract_spans(doc):
+    spans = pd.DataFrame(columns=["xmin", "ymin", "xmax", "ymax", "text", "tag"])
     rows = []
-    for page_num, blocks in Extract_dict(doc).items():
+    for page_num, blocks in extract_dict(doc).items():
         for block in blocks:
-            if block['type'] == 0:
-                for line in block['lines']:
-                    for span in line['spans']:
-                        xmin, ymin, xmax, ymax = list(span['bbox'])
-                        font_size = span['size']
-                        text = unidecode(span['text'])
-                        span_font = span['font']
+            if block["type"] == 0:
+                for line in block["lines"]:
+                    for span in line["spans"]:
+                        xmin, ymin, xmax, ymax = list(span["bbox"])
+                        font_size = span["size"]
+                        text = unidecode(span["text"])
+                        span_font = span["font"]
                         is_upper = False
                         is_bold = False
                         if "bold" in span_font.lower():
                             is_bold = True
                         if re.sub("[\(\[].*?[\)\]]", "", text).isupper():
                             is_upper = True
-                        if text.replace(" ","") !=  "":
-                            rows.append((xmin, ymin, xmax, ymax, text, is_upper, is_bold, span_font, font_size))
-                            span_df = pd.DataFrame(rows, columns=['xmin','ymin','xmax','ymax', 'text', 'is_upper','is_bold','span_font', 'font_size'])
+                        if text.replace(" ", "") != "":
+                            rows.append(
+                                (
+                                    xmin,
+                                    ymin,
+                                    xmax,
+                                    ymax,
+                                    text,
+                                    is_upper,
+                                    is_bold,
+                                    span_font,
+                                    font_size,
+                                )
+                            )
+                            span_df = pd.DataFrame(
+                                rows,
+                                columns=[
+                                    "xmin",
+                                    "ymin",
+                                    "xmax",
+                                    "ymax",
+                                    "text",
+                                    "is_upper",
+                                    "is_bold",
+                                    "span_font",
+                                    "font_size",
+                                ],
+                            )
     return span_df
 
-def Score_span(doc):
+
+def score_span(doc):
     span_scores = []
     span_num_occur = {}
-    special = '[(_:/,#%\=@)&]'
-    for index, span_row in Extract_spans(doc).iterrows():
+    special = "[(_:/,#%\=@)&]"
+    for index, span_row in extract_spans(doc).iterrows():
         score = round(span_row.font_size)
         text = span_row.text
         if not re.search(special, text):
@@ -73,53 +106,124 @@ def Score_span(doc):
         idx += 1
         if size == p_size:
             idx = 0
-            tag[size] = 'p'
+            tag[size] = "p"
         if size > p_size:
-            tag[size] = 'h{0}'.format(idx)
+            tag[size] = "h{0}".format(idx)
         if size < p_size:
-            tag[size] = 's{0}'.format(idx)
+            tag[size] = "s{0}".format(idx)
     span_tags = [tag[score] for score in span_scores]
-    span_df = Extract_spans(doc)
-    span_df['tag'] = span_tags
+    span_df = extract_spans(doc)
+    span_df["tag"] = span_tags
     return span_df
+
 
 def correct_end_line(line):
     "".join(line.rstrip().lstrip())
     if line[-1] == "-":
         return True
-    else :
+    else:
         return False
 
-def Category_text(doc):
+
+def category_text(doc):
     headings_list = []
     text_list = []
     tmp = []
-    heading = ''
-    span_df = Score_span(doc)
+    heading = ""
+    span_df = score_span(doc)
     span_df = span_df.loc[span_df.tag.str.contains("h|p")]
 
     for index, span_row in span_df.iterrows():
         text = span_row.text
         tag = span_row.tag
-        if 'h' in tag:
+        if "h" in tag:
             headings_list.append(text)
-            text_list.append(''.join(tmp))
+            text_list.append("".join(tmp))
             tmp = []
             heading = text
         else:
             if correct_end_line(text):
                 tmp.append(text[:-1])
-            else :
-                tmp.append(text+" ")
+            else:
+                tmp.append(text + " ")
 
-    text_list.append(''.join(tmp))
+    text_list.append("".join(tmp))
     text_list = text_list[1:]
-    text_df = pd.DataFrame(zip(headings_list, text_list),columns=['heading', 'content'])
+    text_df = pd.DataFrame(
+        zip(headings_list, text_list), columns=["heading", "content"]
+    )
     return text_df
 
-def Merge_text(doc):
+
+def merge_text(doc):
     s = ""
-    for index, row in Category_text(doc).iterrows():
-        s += ''.join((row['heading'],'\n',row['content']))
-    return ' '.join(s.split())
-    #return s
+    for index, row in category_text(doc).iterrows():
+        s += "".join((row["heading"], "\n", row["content"]))
+    return clean_text(" ".join(s.split()))
+    # return s
+
+def clean_text(text):
+    out = to_lowercase(text)
+    out = standardize_accented_chars(out)
+    out = remove_url(out)
+    out = expand_contractions(out)
+    out = remove_mentions_and_tags(out)
+    return remove_special_characters(out)
+
+def to_lowercase(text):
+    return text.lower()
+
+
+def standardize_accented_chars(text):
+    return (
+        unicodedata.normalize("NFKD", text)
+        .encode("ascii", "ignore")
+        .decode("utf-8", "ignore")
+    )
+
+
+def remove_url(text):
+    return re.sub(r"https?:\S*", "", text)
+
+
+def expand_contractions(text):
+    expanded_words = []
+    for word in text.split():
+       expanded_words.append(contractions.fix(word))
+    return ' '.join(expanded_words)
+
+def remove_mentions_and_tags(text):
+    text = re.sub(r'@\S*', '', text)
+    return re.sub(r'#\S*', '', text)
+
+def remove_special_characters(text):
+    # define the pattern to keep
+    pat = r'[^a-zA-z0-9.,!?/:;\"\'\s]'
+    return re.sub(pat, '', text)
+
+def remove_punctuation(text):
+    return ''.join([c for c in text if c not in string.punctuation])
+
+
+def remove_stopwords(text, nlp):
+    filtered_sentence = []
+    doc = nlp(text)
+    for token in doc:
+
+        if token.is_stop == False:
+            filtered_sentence.append(token.text)
+    return " ".join(filtered_sentence)
+
+
+def lemmatize(text, nlp):
+   doc = nlp(text)
+   lemmatized_text = []
+   for token in doc:
+     lemmatized_text.append(token.lemma_)
+   return " ".join(lemmatized_text)
+
+
+def extract_keywords(text):
+    kw_extractor = yake.KeywordExtractor(top=20, stopwords=None)
+    keywords = kw_extractor.extract_keywords(text)
+    return [kw for kw, v in keywords]
--- main.py
+++ main.py
@@ -1,18 +1,20 @@
 from Functions import *
+import fitz
 from transformers import pipeline
 
 my_path = "test1.pdf"
 pdf = fitz.open(my_path)
 
-#print(Extract_text(pdf))
-#print(Extract_dict(pdf))
-#print(Extract_spans(pdf))
-#print(Score_span(pdf))
-print(Merge_text(pdf))
+# print(extract_text(pdf))
+# print(extract_dict(pdf))
+# print(extract_spans(pdf))
+# print(score_span(pdf))
+#print(merge_text(pdf))
+print(extract_keywords(merge_text(pdf)))
 
-#summary
+# summary
 # using pipeline API for summarization task
 summarization = pipeline("summarization")
-original_text = Merge_text(pdf)[0:1000]
-summary_text = summarization(original_text)
-print("Summary:\n", summary_text)
+original_text = merge_text(pdf)[0:5000]
+#summary_text = summarization(original_text)
+#print("Summary:\n", summary_text)
